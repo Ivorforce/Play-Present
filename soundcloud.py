bb91cbcda580fd08f62a5bf3d577bd79b5697fbb
Lukas Tenbrink <lukastenbrink@googlemail.com>
2017-11-07 01:35:31 +0100

    Make string search case insensitive

--- soundcloud.py
+++ soundcloud.py
@@ -23,11 +23,11 @@ def try_tracks(tracks, write_out=lambda x: None):
 
         track_url = "https://soundcloud.com" + href
         song_html = requests.get(track_url).text
-        song_title = html.fromstring(song_html).findtext(".//title")
-        purchase_title = purchase_title_regex.search(song_html).group(1)
+        song_title = html.fromstring(song_html).findtext(".//title").lower()
+        purchase_title = purchase_title_regex.search(song_html).group(1).lower()
 
-        if "Free Download" in purchase_title or "Free DL" in purchase_title\
-                or "Free Download" in song_title or "Free DL" in song_title:
+        if "free download" in purchase_title or "free dl" in purchase_title\
+                or "free download" in song_title or "free dl" in song_title:
             track_info = "%s (%s)" % (track, track_url)
             print(track_info)
             write_out(track_info + "\n")
@@ -35,4 +35,3 @@ def try_tracks(tracks, write_out=lambda x: None):
         searched += 1
         if searched % 20 == 0:
             print("Searched %d tracks" % searched)
-
